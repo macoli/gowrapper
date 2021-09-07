247406f133c18da1aa0edb8d9710b35eaa0c49fb
lihui <lihui@lihuideMacBook-Pro.local>
2021-09-07 18:10:26 +0800

    add table

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/macoli/iwrapper
 go 1.17
 
 require (
-	github.com/blinkbean/dingtalk v0.0.0-20210905093040-7d935c0f7e19
+	github.com/alexeyco/simpletable v1.0.0
 	github.com/go-redis/redis/v8 v8.11.3
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/rivo/uniseg v0.1.0 // indirect
 )
---
+++ table/table.go
@@ -0,0 +1,56 @@
+package table
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+
+	"github.com/alexeyco/simpletable"
+)
+
+//ShowTable 通过表格展示数据
+func ShowTable(headerCells []*simpletable.Cell, bodyCells [][]*simpletable.Cell) {
+	// 新建表格
+	table := simpletable.New()
+	// 设置表头
+	table.Header = &simpletable.Header{
+		Cells: headerCells,
+	}
+	// 填充表数据
+	table.Body.Cells = bodyCells
+	// 设置表格样式
+	table.SetStyle(simpletable.StyleUnicode)
+	// 打印表格
+	fmt.Println(table.String())
+}
+
+//GetHeaderCells 生成表头
+func GenHeaderCells(m interface{}) []*simpletable.Cell {
+	t := reflect.TypeOf(m)
+	Cells := []*simpletable.Cell{
+		{Align: simpletable.AlignRight, Text: "ID"},
+	}
+	for i := 0; i < t.NumField(); i++ {
+		cell := simpletable.Cell{Align: simpletable.AlignCenter, Text: t.Field(i).Name}
+		Cells = append(Cells, &cell)
+	}
+	return Cells
+}
+
+//GetBodyCells 生成表数据
+func GenBodyCells(m []interface{}) [][]*simpletable.Cell {
+	var Cells [][]*simpletable.Cell
+	cnt := 1
+	for _, row := range m {
+		var Cell = []*simpletable.Cell{
+			{Align: simpletable.AlignLeft, Text: strconv.Itoa(cnt)},
+		}
+		for _, item := range row.([]string) {
+			c := simpletable.Cell{Align: simpletable.AlignLeft, Text: item}
+			Cell = append(Cell, &c)
+		}
+		Cells = append(Cells, Cell)
+		cnt += 1
+	}
+	return Cells
+}
